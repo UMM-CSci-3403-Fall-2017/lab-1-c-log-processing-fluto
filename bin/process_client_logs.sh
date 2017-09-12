1bd25915c0d1257febdf01586e1e069c57607b4a
scvstackkk <scv.stack@gmail.com>
2017-09-12 11:37:19 -0500

    Modified process_client_logs.sh by changing to the correct directory and getting the awk statement to check the form of the entries from the tar files.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,10 +2,14 @@ target_dir=$1
 
 cd $target_dir
 
-files=$(pwd)/*
+files=var/log/*
 
+echo $(pwd)
+ls
+
+echo "" > failed_login_data.txt
 for i in $files
 do
-    cat $i | grep "Failed password for" | awk '{ print $1, $2 }' >> failed_login_data.txt
+	cat $i | grep "Failed password for" | awk '{ if ($10 =="user") print $1, $2, substr($3, 0, 2), $11, $13; else print $1, $2, substr($3, 0, 2), $9, $11; }' >> failed_login_data.txt
 done
 
