83fd6851415ee3abf1b4c3f0f3d6e880aea81284
scvstackkk <scv.stack@gmail.com>
2017-09-17 21:15:33 -0500

    Add create_country_dist.sh, still need to add comments and cleanup in scripts

---
+++ bin/create_country_dist.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+
+TARGET_DIR=$1
+
+rm -f $TARGET_DIR/country_dist.html
+
+touch failed_login_data_full.txt
+
+echo -n '' > failed_login_data_full.txt
+
+DIRS=$TARGET_DIR/*
+CURRENT_DIR=$(pwd)
+
+for i in $DIRS
+do
+	cat $i/failed_login_data.txt >> $CURRENT_DIR/failed_login_data_full.txt
+done
+
+touch country_ip_addresses.txt
+touch country_ip_codes.txt
+touch country_code_count.txt
+touch country_code_body.html
+
+echo -n '' > country_ip_addresses.txt
+echo -n '' > country_ip_codes.txt
+echo -n '' > country_code_count.txt
+echo -n '' > country_code_body.html
+
+#cat failed_login_data_full.txt | awk '{ print $5 }' | grep etc/country_IP_map.txt 
+cat failed_login_data_full.txt | while read line;
+do
+	IP_ADDRESS=$(echo $line | awk '{ print $5 }')
+	grep "$IP_ADDRESS" etc/country_IP_map.txt | awk '{ print $2 }' >> country_ip_codes.txt
+done
+
+cat country_ip_codes.txt | sort | uniq -c >> country_code_count.txt
+
+cat country_code_count.txt | while read line;
+do
+	COUNTRY_CODE_COUNT=$(echo $line | awk '{ print $1 }')
+	COUNTRY_CODE=$(echo $line | awk '{ print $2 }')
+	echo -n "data.addRow(['" >> country_code_body.html
+    	echo -n $COUNTRY_CODE >> country_code_body.html
+    	echo -n "', " >> country_code_body.html
+    	echo -n $COUNTRY_CODE_COUNT >> country_code_body.html
+    	echo "]);" >> country_code_body.html
+done
+
+cat country_code_body.html
+echo "got here"
+
+./bin/wrap_contents.sh country_code_body.html html_components/country_dist $TARGET_DIR/country_dist.html
+
